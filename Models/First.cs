2324dae3e0da7551ed6d79c55c7353ddebedb7dc
lakshay <bhardwajlakshay545@gmail.com>
2024-04-09 22:55:36 +0530

    Add project files.

---
+++ Models/First.cs
@@ -0,0 +1,9 @@
+﻿namespace webapi_tutorial.Models
+{
+    public class First
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ WeatherForecast.cs
@@ -0,0 +1,10 @@
+namespace webapi_tutorial
+{
+    public class WeatherForecast
+    {
+       public string Name { get; set; }
+        public string  id { get; set; }
+
+        public int age { get; set; }
+    }
+}
